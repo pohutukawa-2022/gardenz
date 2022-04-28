c07b8c252c64c89925b0ca12e6756a82fc40a8a0
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-04-29 11:38:48 +1200

    new produce db functions created

--- server/db/produce.js
+++ server/db/produce.js
@@ -2,7 +2,10 @@ const connection = require('./connection')
 
 module.exports = {
   getProduce,
+  getProduceTypes,
   addProduce,
+  addGardenProduce,
+  listGardenProduce,
 }
 
 function getProduce(db = connection) {
@@ -10,5 +13,37 @@ function getProduce(db = connection) {
 }
 
 function addProduce(newProduce, db = connection) {
-  return db('produce').insert(newProduce)
+  const { name, produceTypeId, status } = newProduce
+  return db('produce').insert({
+    name,
+    produce_type_id: produceTypeId,
+    status,
+  })
 }
+
+function listGardenProduce(db = connection) {
+  return db('garden_produce')
+    .join('produce', 'garden_produce.produce_id', 'produce.id')
+    .join('produce_types', 'produce.produce_type_id', 'produce.id')
+    .select(
+      'produce.id as produceId',
+      'produce.name',
+      'produce.status',
+      'produce_types.id as produceTypeId',
+      'produce_types.name as produceTypeName'
+    )
+}
+
+function getProduceTypes(db = connection) {
+  return db('produce_types').select()
+}
+
+function addGardenProduce(produceId, gardenId, db = connection) {
+  return db('garden_produce').insert({
+    produce_id: produceId,
+    garden_id: gardenId,
+  })
+}
+
+// add and update garden_produce to change the status flag
+// produce status moved to garden_produce
