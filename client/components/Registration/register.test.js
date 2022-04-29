bedd0cd176d8efeaa0b8f5343063a0e7df499950
Clementine <cmwsmart@gmail.com>
2022-04-29 16:28:04 +1200

    Remove unnecessary properties from garden objects in register.test.js

--- client/components/Registration/register.test.js
+++ client/components/Registration/register.test.js
@@ -19,22 +19,10 @@ describe('Register form field', () => {
       {
         id: 1,
         name: 'Kelmarna Gardens',
-        address: '12 Hukanui Crescent',
-        description:
-          'Kelmarna Gardens is a city farm and organic community garden, situated on 4.5 acres of council land in Ponsonby, close to the heart of Auckland City.',
-        lat: -36.85137418793577,
-        lon: 174.73319270646485,
-        url: 'http://www.kelmarnagardens.nz/',
       },
       {
         id: 2,
         name: 'Kingsland Community Orchard',
-        address: '48B Bond Street, Kingsland',
-        description:
-          'A secluded edible oasis in the heart of Kingsland, Auckland. Kingsland Community Orchard; an edible urban garden and orchard located in the heart of Kingsland, Auckland.',
-        lat: -36.86983345249252,
-        lon: 174.74701843955708,
-        url: 'https://www.facebook.com/KCOnz',
       },
     ])
   })
