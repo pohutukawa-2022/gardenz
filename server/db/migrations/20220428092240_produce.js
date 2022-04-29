26fed946888373b4104643d703220c4a977c1d68
mabsam <mabursam@gmail.com>
2022-04-29 16:38:45 +1200

    Write a test for produce.post

--- server/db/migrations/20220428092240_produce.js
+++ server/db/migrations/20220428092240_produce.js
@@ -7,7 +7,6 @@ exports.up = function (knex) {
     table.increments('id')
     table.string('name')
     table.integer('produce_type_id').references('produce_types.id')
-    table.boolean('status')
   })
 }
 
