18fce1979b8b954c2ec1a87fab1c923216246152
HobbesWSL <jessewilson@outlook.com>
2022-05-03 10:26:30 +1200

    add new news button to News page

--- client/components/News/NewsList.jsx
+++ client/components/News/NewsList.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import News from './News'
 
 export default function NewsList({ news }) {
+  const { id: gardenId } = useParams()
+  const isAdmin = useSelector((globalState) => globalState.user.isAdmin)
   return (
-    <ul className="list-primary">
-      {news.map((news) => (
-        <News news={news} key={news.id} />
-      ))}
-    </ul>
+    <>
+      <h1>News</h1>
+      <div className="add-event">
+        {isAdmin ? (
+          <Link to={`/gardens/${gardenId}/news/new`} className="inline-button">
+            Add New News
+          </Link>
+        ) : null}
+      </div>
+      <ul className="list-primary">
+        {news.map((news) => (
+          <News news={news} key={news.id} />
+        ))}
+      </ul>
+    </>
   )
 }
