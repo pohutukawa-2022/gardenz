e6096d491b8c2ae5c8af2c308ec8cea633083c58
HobbesWSL <jessewilson@outlook.com>
2022-05-03 08:31:24 +1200

    add helper to AddNews component to handle submits

--- client/pages/admin/AddNews/addNewsHelper.js
+++ client/pages/admin/AddNews/addNewsHelper.js
@@ -4,26 +4,23 @@ import { showError } from '../../../actions/error'
 import requestor from '../../../consume'
 
 export function addNews(news, navigateTo, consume = requestor) {
-  // const storeState = getState()
-  // const { gardenId, token } = storeState.user
-  // const newEvent = {
-  //   gardenId,
-  //   ...event,
-  // }
-  // dispatch(setWaiting())
-  // return consume('/events', token, 'post', newEvent)
-  //   .then(() => {
-  //     navigateTo(`/gardens/${gardenId}`)
-  //     return null
-  //   })
-  //   .catch((err) => {
-  //     dispatch(showError(err.message))
-  //   })
-  //   .finally(() => {
-  //     dispatch(clearWaiting())
-  //   })
-
-  console.log(news)
-
-
+  const storeState = getState()
+  const { gardenId, token, id } = storeState.user
+  const newEvent = {
+    gardenId,
+    author: id, //can we get this from token on the backend??
+    ...news,
+  }
+  dispatch(setWaiting())
+  return consume(`/news/${gardenId}`, token, 'post', newEvent)
+    .then(() => {
+      navigateTo(`/gardens/${gardenId}/news`)
+      return null
+    })
+    .catch((err) => {
+      dispatch(showError(err.message))
+    })
+    .finally(() => {
+      dispatch(clearWaiting())
+    })
 }
