823f42285e4f04b24859f1d4e41786df46b61095
ahmad <eng.ahmed.anwar@gmail.com>
2022-05-03 10:36:42 +1200

    remove comments

--- server/db/migrations/20220428092237_produceTypes.js
+++ server/db/migrations/20220428092237_produceTypes.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function (knex) {
   return knex.schema.createTable('produce_types', (table) => {
     table.increments('id')
@@ -9,10 +5,6 @@ exports.up = function (knex) {
   })
 }
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
   return knex.schema.dropTable('produce_types')
 }
--- server/db/migrations/20220428092622_gardenProduce.js
+++ server/db/migrations/20220428092622_gardenProduce.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function (knex) {
   return knex.schema.createTable('garden_produce', (table) => {
     table.increments('id')
@@ -11,10 +7,6 @@ exports.up = function (knex) {
   })
 }
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
   return knex.schema.dropTable('garden_produce')
 }
