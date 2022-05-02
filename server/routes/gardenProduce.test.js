c84466570c9cc5933e5292a53c6c14b848daeb32
ahmad <eng.ahmed.anwar@gmail.com>
2022-05-03 11:07:38 +1200

    fix route prefix

--- server/routes/gardenProduce.test.js
+++ server/routes/gardenProduce.test.js
@@ -9,20 +9,14 @@ jest.mock('../logger')
 jest.mock('../db/produce')
 jest.mock('../db/users')
 
-const mockGardenProduce = {
-  produceId: 1,
-  gardenId: 1,
-  status: true,
-}
-
 const testAuthAdminHeader = {
   Authorization: `Bearer ${getAdminToken()}`,
 }
 
-describe('POST /api/v1/garden_produce', () => {
+describe('POST /api/v1/gardenproduce', () => {
   it('responds with status 401 when no token is passed', () => {
     return request(server)
-      .post('/api/v1/garden_produce')
+      .post('/api/v1/gardenproduce')
       .then((res) => {
         expect(res.status).toBe(401)
         return null
@@ -38,7 +32,7 @@ describe('POST /api/v1/garden_produce', () => {
     })
 
     return request(server)
-      .post('/api/v1/garden_produce')
+      .post('/api/v1/gardenproduce')
       .set(testAuthAdminHeader)
       .send({ produceId: 1, gardens: [1, 2] })
       .then((res) => {
@@ -52,7 +46,7 @@ describe('POST /api/v1/garden_produce', () => {
       Promise.reject(new Error('mock addGardenProduce error'))
     )
     return request(server)
-      .post('/api/v1/garden_produce')
+      .post('/api/v1/gardenproduce')
       .set(testAuthAdminHeader)
       .expect('Content-Type', /json/)
       .expect(500)
