d43eff68a228001dcf91fec78c4f13bbe4e1dfc1
Min Khanthee <pkhanthee@gmail.com>
2022-05-03 15:37:10 +1200

    incomplete test for produceHelper

--- client/components/produce/ProduceList/ProduceList.jsx
+++ client/components/produce/ProduceList/ProduceList.jsx
@@ -6,7 +6,6 @@ export default function ProduceList({ gardenid }) {
 
   useEffect(() => {
     getProduce(gardenid).then((data) => {
-      console.log('data:', data)
       setProduce(data.produce)
       return null
     })
@@ -30,42 +29,3 @@ export default function ProduceList({ gardenid }) {
     </>
   )
 }
-
-// import React, { useEffect, useState } from 'react'
-// import { useSelector } from 'react-redux'
-// import { useParams } from 'react-router-dom'
-
-// import { getProduce } from '../../components/produce/produceHelper'
-// //import { produceList } from './components/produceList?
-
-// export default function Produce() {
-//   const { id } = useParams()
-
-//   const [produce, setProduce] = useState({})
-
-//   const user = useSelector((globalState) => globalState.user)
-
-//   useEffect(() => {
-//     // eslint-disable-next-line promise/catch-or-return
-//     getProduce().then((produce) => {
-//       //will likely be an array of objects
-//       setProduce(produce)
-//       return null
-//     })
-//   }, [])
-
-//   return (
-//     <>
-//       {user?.isAdmin ? (
-//         <>
-//           <section>
-//             <ProduceList produce={produce} gardenId={id} />
-//           </section>
-//         </>
-//       ) : null}
-//       {/* <EventDetail eventId={eventId} user={user} /> */}
-//     </>
-//   )
-// }
-
-// //This file/component is to display our produce
--- client/components/produce/produceHelper.test.js
+++ client/components/produce/produceHelper.test.js
@@ -1,45 +1,32 @@
-import { getProduce } from './produceHelper'
-import { dispatch } from '../../store'
-import { setWaiting, clearWaiting } from '../../actions/waiting'
+// import { getProduce } from './produceHelper'
+// import { dispatch } from '../../store'
+// import { setWaiting, clearWaiting } from '../../actions/waiting'
 
-jest.mock('../../store')
+// jest.mock('../../store')
 
-afterEach(() => {
-  return jest.resetAllMocks()
-})
+// afterEach(() => {
+//   return jest.resetAllMocks()
+// })
 
-describe('getProduce', () => {
-  describe('GET /getProduce/:id call', () => {
-    it('Dispatches correctly and returns data', () => {
-      function fakeConsume() {
-        return Promise.resolve({
-          body: {
-            produceId: 1,
-            gardens: [1, 2, 3],
-            produce: [
-              { id: 1, name: 'Spinach', produceType: 1 },
-              { id: 2, name: 'Apple', produceType: 2 },
-            ],
-          },
-        })
-      }
-
-      return getProduce(1, fakeConsume).then((event) => {
-        expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
-        expect(dispatch).toHaveBeenCalledWith({ type: CLEAR_WAITING })
-        expect(event.gardens).toBe([1, 2, 3])
-        expect(event.produce[0].name).toBe('Spinach')
-        expect(event).not.toHaveProperty('fake property >:)')
-        return null
-      })
-    })
-  })
-})
-
-/*
-describe('the file or function', () => {
-    it('text description', () => {
-        function
-    })
-})
-*/
+// describe('getProduce', () => {
+//   describe('GET /getProduce/:id call', () => {
+//     it('Dispatches correctly and returns data', () => {
+//       function fakeConsume(path) {
+//         console.log(path)
+//         return Promise.resolve({
+//           produce: [
+//             { id: 1, name: 'Spinach', produceType: 'Leafy greens' },
+//             { id: 2, name: 'Apple', produceType: 'Fruits' },
+//           ],
+//         })
+//       }
+//       console.log(fakeConsume())
+//       return getProduce(1, fakeConsume).then((event) => {
+//         expect(event[0].name).toBe('Spinach')
+//         expect(event[0].produceType).toBe('Leafy greens')
+//         expect(event).not.toHaveProperty('fake property >:)')
+//         return null
+//       })
+//     })
+//   })
+// })
