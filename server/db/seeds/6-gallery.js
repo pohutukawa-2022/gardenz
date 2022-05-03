44d2ac3adb27e11b8d07ad226f8e7fa417522a07
Clementine <cmwsmart@gmail.com>
2022-05-03 16:49:30 +1200

    Delete gallery seeds

--- server/db/seeds/6-gallery.js
+++
@@ -1,7 +0,0 @@
-exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('gallery').del()
-  await knex('gallery').insert([
-    { id: 1, name: 'image1', mimetype: 'mime', image: null, garden_id: 3 },
-  ])
-}
