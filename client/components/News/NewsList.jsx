42740d9d9f070711e114ebbc50a41bf789a07d1a
ahmad <eng.ahmed.anwar@gmail.com>
2022-05-05 08:14:17 +1200

    handle conditional rendering

--- client/components/News/NewsList.jsx
+++ client/components/News/NewsList.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import News from './News'
+import Conditional from '../Conditional'
 
 export default function NewsList({ news }) {
   const { id: gardenId } = useParams()
   const isAdmin = useSelector((globalState) => globalState.user.isAdmin)
+
   return (
     <>
       <h1>News</h1>
       <div className="add-event">
-        {isAdmin ? (
+        <Conditional condition={isAdmin}>
           <Link to={`/gardens/${gardenId}/news/new`} className="inline-button">
             Add New News
           </Link>
-        ) : null}
+        </Conditional>
       </div>
       <ul className="list-primary">
         {news.map((news) => (
