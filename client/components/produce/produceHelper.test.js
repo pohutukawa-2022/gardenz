a1dcda12719f850419921974c37b1470e85e4563
ahmad <eng.ahmed.anwar@gmail.com>
2022-05-06 05:53:52 +1200

    add test

--- client/components/produce/produceHelper.test.js
+++ client/components/produce/produceHelper.test.js
@@ -1,32 +1,44 @@
-// import { getProduce } from './produceHelper'
-// import { dispatch } from '../../store'
-// import { setWaiting, clearWaiting } from '../../actions/waiting'
+import { getProduce } from './produceHelper'
+import { dispatch } from '../../store'
+import { CLEAR_WAITING, SET_WAITING } from '../../actions/waiting'
 
-// jest.mock('../../store')
+jest.mock('../../store')
 
-// afterEach(() => {
-//   return jest.resetAllMocks()
-// })
+afterEach(() => {
+  return jest.resetAllMocks()
+})
 
-// describe('getProduce', () => {
-//   describe('GET /getProduce/:id call', () => {
-//     it('Dispatches correctly and returns data', () => {
-//       function fakeConsume(path) {
-//         console.log(path)
-//         return Promise.resolve({
-//           produce: [
-//             { id: 1, name: 'Spinach', produceType: 'Leafy greens' },
-//             { id: 2, name: 'Apple', produceType: 'Fruits' },
-//           ],
-//         })
-//       }
-//       console.log(fakeConsume())
-//       return getProduce(1, fakeConsume).then((event) => {
-//         expect(event[0].name).toBe('Spinach')
-//         expect(event[0].produceType).toBe('Leafy greens')
-//         expect(event).not.toHaveProperty('fake property >:)')
-//         return null
-//       })
-//     })
-//   })
-// })
+describe('getProduce', () => {
+  it('Dispatches correctly and returns data', () => {
+    function fakeConsume() {
+      return Promise.resolve({
+        body: {
+          produce: [
+            { id: 1, name: 'Spinach', produceType: 'Leafy greens' },
+            { id: 2, name: 'Apple', produceType: 'Fruits' },
+          ],
+        },
+      })
+    }
+
+    return getProduce(1, fakeConsume).then(({ produce }) => {
+      expect(produce[0].name).toBe('Spinach')
+      expect(produce[0].produceType).toBe('Leafy greens')
+      expect(dispatch).toHaveBeenCalledTimes(2)
+      return null
+    })
+  })
+
+  it('dispatches showError', () => {
+    function fakeConsume() {
+      return Promise.reject(Error('mock error'))
+    }
+
+    return getProduce(1, fakeConsume).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+      expect(dispatch).not.toHaveBeenCalledWith({ type: CLEAR_WAITING })
+      expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
+      return null
+    })
+  })
+})
