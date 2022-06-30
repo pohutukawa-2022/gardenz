eea91b043efd74b5d8387f02d89543299d405cef
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-01 11:38:05 +1200

    migrate to rtk

--- client/store.js
+++ client/store.js
@@ -1,11 +1,10 @@
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
-import reducers from './reducers'
+import reducers from './slices'
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = configureStore({
+  reducer: reducers,
+})
 
 export default store
 export const dispatch = store.dispatch
