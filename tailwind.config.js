bfe5bcb253d3f3559699139e8a34a63a9c85c908
Prue Singh <pruesinghnagra@gmail.com>
2022-07-01 15:19:59 +1200

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./server/**/*.html', './client/**/*.jsx'],
+  media: false,
+  theme: {
+    extend: {
+      colors: {
+        puce: '#d11d03',
+        green: '#8adb55',
+        darkGreen: '#17a17b',
+        lightGreen: '#b8dfd8',
+        orange: '#e5890a',
+        red: '#d11d05',
+        black: '#363636',
+        darkBlue: '#172450',
+        blue: '#99ddcc',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
