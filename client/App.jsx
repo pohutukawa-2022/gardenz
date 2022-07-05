c0efb4d04065e9ddb5cf7f40e3de670167a25762
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-05 17:27:33 +1200

    fix routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
-import { AnimatePresence } from 'framer-motion'
 
 import Header from './subcomponents/Header'
 import Profile from './views/user/Profile/Profile'
-import Garden from './views/user/Garden/Garden'
-import Home from './views/user/Home/Home'
+import About from './views/user/Gardens/about/About'
+import Home from './views/Index/Index'
 import AddEvent from './views/admin/AddEvent/AddEvent'
 import EditEvent from './views/admin/EditEvent/EditEvent'
 import AddGarden from './views/admin/AddGarden/AddGarden'
 import Error from './subcomponents/Error/Error'
 import Event from './views/admin/Event/Event'
-import Gardens from './views/user/GardensList/GardensList'
+import Gardens from './views/user/Gardens/Index/Index'
 import News from './views/user/News/News'
 import AddNews from './views/admin/AddNews/AddNews'
 import Volunteers from './views/user/Volunteers/Volunteers'
@@ -39,34 +38,32 @@ export default function App() {
         <p>this is user</p>
       </IsUser>
       <main className="container margin-container flex-container centre-flex">
-        <AnimatePresence exitBeforeEnter>
-          <IsAdmin>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/gardens" element={<Gardens />} />
-              <Route path="/gardens/new" element={<AddGarden />} />
-              <Route path="/gardens/:id" element={<Garden />} />
-              <Route path="/gardens/:id/news" element={<News />} />
-              <Route path="/gardens/:id/news/new" element={<AddNews />} />
-              <Route path="/gardens/:id/events/:eventId" element={<Event />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/event/new" element={<AddEvent />} />
-              <Route path="/events/:id/edit" element={<EditEvent />} />
-              <Route path="/events/:id/volunteers" element={<Volunteers />} />
-              <Route path="/produce/new" element={<AddProduce />} />
-            </Routes>
-          </IsAdmin>
-          <IsUser>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/gardens" element={<Gardens />} />
-              <Route path="/gardens/:id" element={<Garden />} />
-              <Route path="/gardens/:id/news" element={<News />} />
-              <Route path="/gardens/:id/events/:eventId" element={<Event />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </IsUser>
-        </AnimatePresence>
+        <IsAdmin key="admin">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gardens/new" element={<AddGarden />} />
+            <Route path="/gardens/:id/news" element={<News />} />
+            <Route path="/gardens/:id/news/new" element={<AddNews />} />
+            <Route path="/gardens/:id/events/:eventId" element={<Event />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/event/new" element={<AddEvent />} />
+            <Route path="/events/:id/edit" element={<EditEvent />} />
+            <Route path="/events/:id/volunteers" element={<Volunteers />} />
+            <Route path="/produce/new" element={<AddProduce />} />
+            <Route path="*" element={<p>this page doesnt exist</p>} />
+          </Routes>
+        </IsAdmin>
+        <IsUser key="user">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gardens" element={<Gardens />} />
+            <Route path="/gardens/:id/about" element={<About />} />
+            <Route path="/gardens/:id/news" element={<News />} />
+            <Route path="/gardens/:id/events/:eventId" element={<Event />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<p>this page doesnt exist</p>} />
+          </Routes>
+        </IsUser>
       </main>
     </>
   )
