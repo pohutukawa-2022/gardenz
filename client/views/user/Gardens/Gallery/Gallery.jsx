b9ac0fe266906ba9cac0b203c7e5577767158baa
Mano <m.raankin@gmail.com>
2022-07-07 10:08:00 +1200

    hard coded 2 images

--- client/views/user/Gardens/Gallery/Gallery.jsx
+++ client/views/user/Gardens/Gallery/Gallery.jsx
@@ -5,6 +5,8 @@ function Gallery() {
     <>
       <div>
         <p> hi, this is the gallery</p>
+        <img src="/images/comGardenPlant.png" alt="garden image1" />
+        <img src="/images/comGardenRows.png" alt="garden image2" />
       </div>
     </>
   )
