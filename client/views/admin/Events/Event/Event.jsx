e222bd8c9eaf547a955e6e99151b1fa8fa59de2b
Lucas Norris <luke.norris22@gmail.com>
2022-07-08 11:53:32 +1200

    creat button to link to addEvent page and button to link to editEvent page

--- client/views/admin/Events/Event/Event.jsx
+++ client/views/admin/Events/Event/Event.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { getEvent } from './eventHelper'
 
@@ -9,11 +9,21 @@ import AddVolunteerForm from '../../../../subcomponents/volunteers/RockUpVolunte
 import RockUpVolunteerList from '../../../../subcomponents/volunteers/RockUpVolunteerList/RockUpVolunteerList'
 
 export default function Event() {
+  const navigate = useNavigate()
+
   const { id } = useParams()
 
   const [event, setEvent] = useState({})
 
   const user = useSelector((globalState) => globalState.user)
+
+  const navigateAdd = () => {
+    navigate('/admin/events/add')
+  }
+
+  const navigateEdit = () => {
+    navigate(`/admin/events/${id}/edit`)
+  }
 
   useEffect(() => {
     // eslint-disable-next-line promise/catch-or-return
@@ -31,10 +41,16 @@ export default function Event() {
   }
 
   return (
-    <section>
-      <VolunteerList volunteers={event.volunteers} eventId={event.id} />
-      <RockUpVolunteerList extraVolunteers={event.extraVolunteers} />
-      <AddVolunteerForm addExtraVolunteer={addExtraVolunteer} id={id} />
-    </section>
+    <>
+      <section>
+        <VolunteerList volunteers={event.volunteers} eventId={event.id} />
+        <RockUpVolunteerList extraVolunteers={event.extraVolunteers} />
+        <AddVolunteerForm addExtraVolunteer={addExtraVolunteer} id={id} />
+      </section>
+      <button onClick={navigateAdd}> Add Event</button>
+      <section>
+        <button onClick={navigateEdit}>Edit Event</button>
+      </section>
+    </>
   )
 }
