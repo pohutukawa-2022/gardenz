b031310eb9b96c2a707791df5efb3c8a4933f2a3
Lucas <luke.norris22@gmail.com>
2022-07-07 14:25:55 +1200

    nav bar now shows active page

--- client/subcomponents/userNav/UserNav.jsx
+++ client/subcomponents/userNav/UserNav.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React from 'react'
+import { useParams, NavLink } from 'react-router-dom'
 //import { useSelector } from 'react-redux'
 
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { IoClose } from 'react-icons/io5'
+// import { GiHamburgerMenu } from 'react-icons/gi'
+// import { IoClose } from 'react-icons/io5'
 
 export default function userNav() {
-  const [open, setOpen] = useState(false)
+  //const [open, setOpen] = useState(false)
   const params = useParams()
   const gardenId = params.id
+  //const [isActive, setIsActive] = useState('text-white')
 
-  const toggleMenu = () => {
-    setOpen((prev) => !prev)
-  }
+  //const activeLink = () => {
+  //   setIsActive('text-orange')
+  // }
+
+  // const toggleMenu = () => {
+  //   setOpen((prev) => !prev)
+  // }
 
   return (
     <>
@@ -25,47 +30,62 @@ export default function userNav() {
         }
       >
         <div className="lg:flex text-center md:text-align bg-lightGreen">
-          <Link
+          <NavLink
             to={`/gardens/${gardenId}/about`}
-            className="text-white hover:text-green block my-6 py-2 px-6 "
+            className={({ isActive }) =>
+              (isActive ? 'text-orange' : 'text-white') +
+              ' hover:text-green block my-6 py-2 px-6 '
+            }
           >
             About Us
-          </Link>
+          </NavLink>
 
-          <Link
+          <NavLink
             to={`/gardens/${gardenId}/events`}
-            className="text-white hover:text-green block my-6 py-2 px-6"
+            className={({ isActive }) =>
+              (isActive ? 'text-orange' : 'text-white') +
+              ' hover:text-green block my-6 py-2 px-6 '
+            }
           >
             Events
-          </Link>
+          </NavLink>
 
-          <Link
+          <NavLink
             to={`/gardens/${gardenId}/news`}
-            className="text-white hover:text-green block my-6 py-2 px-6"
+            className={({ isActive }) =>
+              (isActive ? 'text-orange' : 'text-white') +
+              ' hover:text-green block my-6 py-2 px-6 '
+            }
           >
             News
-          </Link>
+          </NavLink>
 
-          <Link
+          <NavLink
             to={`/gardens/${gardenId}/gallery`}
-            className="text-white hover:text-green block my-6 py-2 px-6"
+            className={({ isActive }) =>
+              (isActive ? 'text-orange' : 'text-white') +
+              ' hover:text-green block my-6 py-2 px-6 '
+            }
           >
             Gallery
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to={`/gardens/${gardenId}/shop`}
-            className="text-white hover:text-green block my-6 py-2 px-6"
+            className={({ isActive }) =>
+              (isActive ? 'text-orange' : 'text-white') +
+              ' hover:text-green block my-6 py-2 px-6 '
+            }
           >
             Shop
-          </Link>
+          </NavLink>
         </div>
       </nav>
-      <div
+      {/* <div
         className="lg:hidden z-90 absolute top-4 right-6 text-white text-4xl"
         onClick={toggleMenu}
       >
         {open ? <IoClose /> : <GiHamburgerMenu />}
-      </div>
+      </div> */}
     </>
   )
 }
