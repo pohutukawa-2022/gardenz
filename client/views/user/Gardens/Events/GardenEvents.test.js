223c6a9127e93edc1d11cdf67e852429dd983b77
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-08 14:55:09 +1200

    add expects

--- client/views/user/Gardens/Events/GardenEvents.test.js
+++ client/views/user/Gardens/Events/GardenEvents.test.js
@@ -7,34 +7,41 @@ import { screen } from '@testing-library/react'
 
 jest.mock('../about/aboutHelper')
 
-test('garden events should render correct list of events', () => {
+test('garden events should render correct list of events', async () => {
   getGarden.mockImplementation(() => {
-    return Promise.resolve({
-      name: 'name',
-      events: [
-        {
-          id: 1,
-          volunteersNeeded: 5,
-          title: 'test event',
-          date: 'date',
-          description: 'description',
-          status: 'status',
-          // volunteer: {
-          //   userId: event.userId,
-          // },
-        },
-      ],
-    })
+    return Promise.resolve()
   })
+
   renderWithRedux(<GardenEvents />, {
     initialEntries: ['/gardens/1/events'],
     route: '/gardens/:id/events',
-    initialState: { user: { id: 1 } },
+    initialState: {
+      user: { id: 1 },
+      garden: {
+        name: 'name',
+        address: 'address',
+        events: [
+          {
+            id: 1,
+            volunteersNeeded: 10,
+            totalVolunteers: 5,
+            title: 'event title',
+            date: 'date',
+            description: 'description',
+            status: 'status',
+          },
+        ],
+      },
+    },
   })
-  return screen.findAllByRole('article').then((events) => {
-    expect(getGarden).toHaveBeenCalledTimes(1)
-    console.log(events)
-    expect(events[0].textContent).toMatch('test event')
-    return null
-  })
+
+  expect(getGarden).toHaveBeenCalledTimes(1)
+  const title = await screen.findByText('event title')
+  const date = await screen.findByText('date')
+  const address = await screen.findByText('address')
+  const remaining = await screen.findByText('5 of 10')
+  expect(title).toBeInTheDocument()
+  expect(date).toBeInTheDocument()
+  expect(address).toBeInTheDocument()
+  expect(remaining).toBeInTheDocument()
 })
