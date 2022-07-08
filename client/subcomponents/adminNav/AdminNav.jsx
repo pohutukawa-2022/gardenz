dfc0dd0bc40ccf07fa7ffea982d5e44cdaeef7ed
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-08 17:52:00 +1200

    fix links

--- client/subcomponents/adminNav/AdminNav.jsx
+++ client/subcomponents/adminNav/AdminNav.jsx
@@ -13,22 +13,22 @@ function AdminNav() {
       <nav className=" py-2 text-center bg-orange">
         <div className="lg:flex justify-between md:text-align text-white ">
           <div className="pl-4">
-            <Link to={`admin/gardens/${id}/produce`}>Update Produce List</Link>
+            <Link to={`/admin/gardens/${id}/produce`}>Update Produce List</Link>
           </div>
           <div>
-            <Link to={`admin/events/${id}`}> Add/Edit Events </Link>
+            <Link to={`/admin/events/${id}`}> Add/Edit Events </Link>
           </div>
           <div>
-            <Link to={`gardens/${id}/news/add`}>Post News </Link>
+            <Link to={`/gardens/${id}/news/add`}>Post News </Link>
           </div>
           <div>
-            <Link to={`events/${id}/volunteers`}>Update Gallery </Link>
+            <Link to={`/events/${id}/volunteers`}>Update Gallery </Link>
           </div>
           <div>
-            <Link to={`events/${id}/volunteers`}>Manage Order </Link>
+            <Link to={`/events/${id}/volunteers`}>Manage Order </Link>
           </div>
           <div className="pr-4">
-            <Link to={`admin/gardens`}>Change Garden </Link>
+            <Link to={`/admin/gardens`}>Change Garden </Link>
           </div>
         </div>
       </nav>
