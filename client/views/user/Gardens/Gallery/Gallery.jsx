59bb094f9275c101b3a0d25a6e5853f7fcaaab6d
Jingjing <jlkjfk@hotmail.com>
2022-07-11 15:01:22 +1200

    Gallery.jsx - changed main to section

--- client/views/user/Gardens/Gallery/Gallery.jsx
+++ client/views/user/Gardens/Gallery/Gallery.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Gallery() {
   return (
     <>
-      <main>
+      <section>
         <p> hi, this is the gallery</p>
         <img src="/images/comGardenPlant.png" alt="garden image1" />
         <img src="/images/comGardenRows.png" alt="garden image2" />
-      </main>
+      </section>
     </>
   )
 }
