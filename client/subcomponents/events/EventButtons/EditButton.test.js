03571b26dfd7e1a122ce5636a52522af353c4561
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-12 06:27:18 +1200

    improve test

--- client/subcomponents/events/EventButtons/EditButton.test.js
+++ client/subcomponents/events/EventButtons/EditButton.test.js
@@ -1,11 +1,8 @@
 import React from 'react'
-
 import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { waitFor } from '@testing-library/dom'
 
 import EditButton from './EditButton'
-import { renderWithRouter } from '../../../test-utils'
 
 const mockedUsedNavigate = jest.fn()
 jest.mock('react-router-dom', () => ({
@@ -20,15 +17,13 @@ describe('edit button', () => {
     expect(addButton).toHaveTextContent('Edit Event')
   })
   it('redirects to /admin/events/:id/edit on click', async () => {
-    renderWithRouter(<EditButton />, {
+    render(<EditButton eventId={1} />, {
       initialEntries: ['/admin/events/1'],
       route: '/admin/events/:id',
     })
     const editButton = screen.getByRole('button')
     userEvent.click(editButton)
 
-    await waitFor(() => {
-      expect(mockedUsedNavigate).toHaveBeenCalledWith(`/admin/events/1/edit`)
-    })
+    expect(mockedUsedNavigate).toHaveBeenCalledWith(`/admin/events/1/edit`)
   })
 })
