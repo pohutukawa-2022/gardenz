98c42c9da9f275347ca28f1bf8df7286d9a68c2f
Jingjing <jlkjfk@hotmail.com>
2022-07-11 16:34:55 +1200

    complete map.test.js - displays correct number of markers from coordinates prop

--- client/subcomponents/Map/Map.test.js
+++ client/subcomponents/Map/Map.test.js
@@ -18,7 +18,6 @@ describe('user location marker', () => {
       />
     )
     const marker = await screen.findByRole('presentation')
-    console.log(marker, 'THIS IS MARKER')
     expect(marker).toBeInTheDocument()
   })
   it('user location marker has a different image', async () => {
@@ -47,29 +46,30 @@ describe('user location marker', () => {
   })
 })
 
-// describe.skip('garden location markers', () => {
-//   it('displays correct number of markers from coordinates prop', async () => {
-//     const coordinates = [
-//       {
-//         lat: -36.86667,
-//         lon: 174.76667,
-//       },
-//       {
-//         lat: -36.8888888,
-//         lon: 174.7777777,
-//       },
-//     ]
-//     render(
-//       <Map
-//         addresses={['address 1', 'address 2']}
-//         names={['name 1', 'name 2']}
-//         coordinates={coordinates}
-//       />
-//     )
-//     const markers = await screen.findByRole('img')
-
-//     // there are 2 marker images per marker...
-//     // each marker has a marker image and the marker's shadow image
-//     expect(markers).toHaveLength(4)
-//   })
-// })
+describe('garden location markers', () => {
+  it('displays correct number of markers from coordinates prop', async () => {
+    const coordinates = [
+      {
+        lat: -36.86667,
+        lon: 174.76667,
+      },
+      {
+        lat: -36.8888888,
+        lon: 174.7777777,
+      },
+    ]
+    render(
+      <Map
+        addresses={['address 1', 'address 2']}
+        names={['name 1', 'name 2']}
+        coordinates={coordinates}
+      />
+    )
+    const markers = await screen.findAllByRole('img')
+    const markersShadow = await screen.findAllByAltText('Marker')
+    // there are 2 marker images per marker...
+    // each marker has a marker image and the marker's shadow image
+    expect(markers).toHaveLength(2)
+    expect(markersShadow).toHaveLength(2)
+  })
+})
