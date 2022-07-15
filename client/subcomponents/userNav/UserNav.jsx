a319c1c9f7a49a09e65f73f8240879116579d424
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-15 13:44:51 +1200

    update userNav

--- client/subcomponents/userNav/UserNav.jsx
+++ client/subcomponents/userNav/UserNav.jsx
@@ -5,59 +5,57 @@ export default function UserNav() {
   const { id } = useParams()
 
   return (
-    <>
-      <nav>
-        <div className="lg:flex flex-row text-center md:text-align bg-lightGreen">
-          <NavLink
-            to={`/gardens/${id}/about`}
-            className={({ isActive }) =>
-              (isActive ? 'text-orange' : 'text-white') +
-              ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
-            }
-          >
-            About Us
-          </NavLink>
+    <nav>
+      <div className="lg:flex flex-row text-center md:text-align bg-lightGreen">
+        <NavLink
+          to={`/gardens/${id}/about`}
+          className={({ isActive }) =>
+            (isActive ? 'text-orange' : 'text-white') +
+            ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
+          }
+        >
+          About Us
+        </NavLink>
 
-          <NavLink
-            to={`/gardens/${id}/events`}
-            className={({ isActive }) =>
-              (isActive ? 'text-orange' : 'text-white') +
-              ' hover:text-green block my-6  py-2 px-6 basis-1/4  '
-            }
-          >
-            Events
-          </NavLink>
+        <NavLink
+          to={`/gardens/${id}/events`}
+          className={({ isActive }) =>
+            (isActive ? 'text-orange' : 'text-white') +
+            ' hover:text-green block my-6  py-2 px-6 basis-1/4  '
+          }
+        >
+          Events
+        </NavLink>
 
-          <NavLink
-            to={`/gardens/${id}/news`}
-            className={({ isActive }) =>
-              (isActive ? 'text-orange' : 'text-white') +
-              ' hover:text-green block my-6 py-2 px-6 basis-1/4 '
-            }
-          >
-            News
-          </NavLink>
+        <NavLink
+          to={`/gardens/${id}/news`}
+          className={({ isActive }) =>
+            (isActive ? 'text-orange' : 'text-white') +
+            ' hover:text-green block my-6 py-2 px-6 basis-1/4 '
+          }
+        >
+          News
+        </NavLink>
 
-          <NavLink
-            to={`/gardens/${id}/gallery`}
-            className={({ isActive }) =>
-              (isActive ? 'text-orange' : 'text-white') +
-              ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
-            }
-          >
-            Gallery
-          </NavLink>
-          <NavLink
-            to={`/gardens/${id}/shop`}
-            className={({ isActive }) =>
-              (isActive ? 'text-orange' : 'text-white') +
-              ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
-            }
-          >
-            Shop
-          </NavLink>
-        </div>
-      </nav>
-    </>
+        <NavLink
+          to={`/gardens/${id}/gallery`}
+          className={({ isActive }) =>
+            (isActive ? 'text-orange' : 'text-white') +
+            ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
+          }
+        >
+          Gallery
+        </NavLink>
+        <NavLink
+          to={`/gardens/${id}/shop`}
+          className={({ isActive }) =>
+            (isActive ? 'text-orange' : 'text-white') +
+            ' hover:text-green block my-6  py-2 px-6 basis-1/4 '
+          }
+        >
+          Shop
+        </NavLink>
+      </div>
+    </nav>
   )
 }
