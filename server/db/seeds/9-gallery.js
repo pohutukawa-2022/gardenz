9fde81695aeff8687adb4da0a622b73cf24b6166
ahmad <eng.ahmed.anwar@gmail.com>
2022-07-26 15:15:56 +1200

    fix tests

--- server/db/seeds/9-gallery.js
+++ server/db/seeds/9-gallery.js
@@ -4,19 +4,19 @@ exports.seed = async function (knex) {
     {
       id: 1,
       name: 'image1',
-      url: '',
+      url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/dji-0065_orig.jpg',
       garden_id: 1,
     },
     {
       id: 2,
       name: 'image2',
-      url: '',
+      url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/kelmarna-autumn-festival-2019-2-1_orig.jpg',
       garden_id: 3,
     },
     {
       id: 3,
       name: 'image3',
-      url: '',
+      url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/published/kelmarnagardens-5.jpg?1605756669',
       garden_id: 2,
     },
   ])
