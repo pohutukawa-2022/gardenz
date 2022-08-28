dddf252745fa26c1a501cd456be3473981d1019e
ahmad <eng.ahmed.anwar@gmail.com>
2022-08-29 07:21:21 +1200

    run lint

--- client/views/user/Profile/Profile.jsx
+++ client/views/user/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import Register from '../../../subcomponents/Registration/Register'
 import { View } from '../../../subcomponents/Registration/View'
