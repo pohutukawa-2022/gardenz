13a48f6d86411dc9ec667a90327b4c86fa9238eb
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-06 06:56:50 +1200

    add new links to admin routes

--- client/subcomponents/adminNav/AdminNav.jsx
+++ client/subcomponents/adminNav/AdminNav.jsx
@@ -5,29 +5,31 @@ function AdminNav() {
   const { id } = useParams()
 
   return (
-    <>
-      <nav>
-        <nav className=" py-2 text-center bg-orange">
-          <ul className="lg:flex justify-between md:text-align text-white ">
-            <li>
-              <Link to={`/admin/gardens/${id}/events`}> Add/Edit Events </Link>
-            </li>
-            <li>
-              <Link to={`/admin/gardens/${id}/news/add`}>Post News </Link>
-            </li>
-            <li>
-              <Link to={`/admin/gardens/${id}/gallery`}>Update Gallery </Link>
-            </li>
-            <li>
-              <Link to={`/admin/gardens/${id}/orders`}>Manage Order </Link>
-            </li>
-            <li className="pr-4">
-              <Link to='/admin/gardens'>Change Garden </Link>
-            </li>
-          </ul>
-        </nav>
-      </nav>
-    </>
+    <nav className=" py-2 text-center bg-orange px-4">
+      <ul className="lg:flex justify-between md:text-align text-white ">
+        <li>
+          <Link to={`/admin/gardens/${id}/cover`}>Update Cover</Link>
+        </li>
+        <li>
+          <Link to={`/admin/gardens/${id}/events`}>Add/Edit Events</Link>
+        </li>
+        <li>
+          <Link to={`/admin/gardens/${id}/news/add`}>Post News</Link>
+        </li>
+        <li>
+          <Link to={`/admin/gardens/${id}/gallery`}>Update Gallery</Link>
+        </li>
+        <li>
+          <Link to={`/admin/gardens/${id}/orders`}>Manage Order</Link>
+        </li>
+        <li>
+          <Link to={`/admin/gardens/${id}/volunteers`}>Volunteers</Link>
+        </li>
+        <li className="pr-4">
+          <Link to="/admin/gardens">Change Garden</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
