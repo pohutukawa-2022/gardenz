0cab198ab97abf4d469250c3fef750e94f3f90ee
Gracie <gg.hhayward@gmail.com>
2022-09-15 10:44:02 +1200

    useGardenHelper .then to async await

--- client/hooks/useGarden/useGardenHelper.js
+++ client/hooks/useGarden/useGardenHelper.js
@@ -1,29 +1,44 @@
 import consume from '../../consume'
 
-export async function  getGarden(id) {
+export async function getGarden(id) {
   const headers = {
     Accept: 'application/json',
   }
   try {
-    const garden = await res.body
-    
-    // return consume(`/gardens/${id}`, '', 'get', {}, headers)
-      // return {
-      //   name: garden.name,
-      //   description: garden.description,
-      //   address: garden.address,
-      //   imageHeaderUrl: garden.imageHeaderUrl,
-      //   events: garden.events,
-      //   lat: garden.lat,
-      //   lon: garden.lon,
-      //   phone: garden.phone,
-      //   email: garden.email,
-      // }
-    }  catch (err) {
-       res.status(500).json({
-          error: {
-            title: 'Unable to retrieve event',
-          },
-        })
-      }
+    const res = await consume(`/gardens/${id}`, '', 'get', {}, headers)
+    const garden = res.body
+    return {
+      name: garden.name,
+      description: garden.description,
+      address: garden.address,
+      imageHeaderUrl: garden.imageHeaderUrl,
+      events: garden.events,
+      lat: garden.lat,
+      lon: garden.lon,
+      phone: garden.phone,
+      email: garden.email,
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+// export function getGarden(id) {
+//   const headers = {
+//     Accept: 'application/json',
+//   }
+//   return consume(`/gardens/${id}`, '', 'get', {}, headers).then((res) => {
+//     const garden = res.body
+//     return {
+//       name: garden.name,
+//       description: garden.description,
+//       address: garden.address,
+//       imageHeaderUrl: garden.imageHeaderUrl,
+//       events: garden.events,
+//       lat: garden.lat,
+//       lon: garden.lon,
+//       phone: garden.phone,
+//       email: garden.email,
+//     }
+//   })
+// }
--- client/subcomponents/produce/ProduceList/produceHelper.js
+++ client/subcomponents/produce/ProduceList/produceHelper.js
@@ -4,17 +4,6 @@ import { setWaiting, clearWaiting } from '../../../slices/waiting'
 import { showError } from '../../../slices/error'
 
 export async function getProduce(gardenid, consume = requestor) {
-  //   dispatch(setWaiting())
-  //   return consume(`/gardenproduce/${gardenid}`)
-  //     .then((res) => {
-  //       dispatch(clearWaiting())
-  //       return res.body
-  //     })
-  //     .catch((error) => {
-  //       dispatch(showError(error.message))
-  //     })
-  // // }
-
   dispatch(setWaiting())
   try {
     const res = await consume(`/gardenproduce/${gardenid}`)
