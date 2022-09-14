7dec4bd257385d145a9039b91dafe9501359c609
Frey <freymccahonjones@gmail.com>
2022-09-15 10:31:20 +1200

    change .then to async await

--- client/views/admin/Gardens/AddGarden/addGardenHelper.js
+++ client/views/admin/Gardens/AddGarden/addGardenHelper.js
@@ -3,29 +3,13 @@ import { getState, dispatch } from '../../../../store'
 import { clearWaiting, setWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 
-//create a post api function to post to the server-side-route
-// export function addGarden(data, consume = requestor) {
-//   const storeState = getState()
-//   const { token } = storeState.user
-
-//   dispatch(setWaiting())
-//   return consume('/gardens', token, 'post', data)
-//     .then(() => {
-//       dispatch(clearWaiting())
-//       return null
-//     })
-//     .catch((error) => {
-//       dispatch(showError(error.message))
-//     })
-// }
-
 export async function addGarden(data, consume = requestor) {
   const storeState = getState()
   const { token } = storeState.user
 
   dispatch(setWaiting())
-  await consume('/gardens', token, 'post', data)
   try {
+    await consume('/gardens', token, 'post', data)
     dispatch(clearWaiting())
     return null
   } catch (error) {
