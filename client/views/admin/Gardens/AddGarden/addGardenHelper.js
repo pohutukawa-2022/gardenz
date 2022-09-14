f108e3fd91956a97aa9fbce150dcefaf32beb862
Ford <Fordnicholasjones@gmail.com>
2022-09-14 17:29:10 +1200

    mid way through addGardenHelper

--- client/views/admin/Gardens/AddGarden/addGardenHelper.js
+++ client/views/admin/Gardens/AddGarden/addGardenHelper.js
@@ -4,17 +4,31 @@ import { clearWaiting, setWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 
 //create a post api function to post to the server-side-route
-export function addGarden(data, consume = requestor) {
+// export function addGarden(data, consume = requestor) {
+//   const storeState = getState()
+//   const { token } = storeState.user
+
+//   dispatch(setWaiting())
+//   return consume('/gardens', token, 'post', data)
+//     .then(() => {
+//       dispatch(clearWaiting())
+//       return null
+//     })
+//     .catch((error) => {
+//       dispatch(showError(error.message))
+//     })
+// }
+
+export async function addGarden(data, consume = requestor) {
   const storeState = getState()
   const { token } = storeState.user
 
   dispatch(setWaiting())
-  return consume('/gardens', token, 'post', data)
-    .then(() => {
-      dispatch(clearWaiting())
-      return null
-    })
-    .catch((error) => {
-      dispatch(showError(error.message))
-    })
+  await consume('/gardens', token, 'post', data)
+  try {
+    dispatch(clearWaiting())
+    return null
+  } catch (error) {
+    dispatch(showError(error.message))
+  }
 }
