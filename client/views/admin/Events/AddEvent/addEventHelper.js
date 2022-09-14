b1dc8bb48888f5ff115c069b86d6d6926c4fefcc
Yara <yaragagen@gmail.com>
2022-09-15 09:38:01 +1200

    finish addEventHelper

--- client/views/admin/Events/AddEvent/addEventHelper.js
+++ client/views/admin/Events/AddEvent/addEventHelper.js
@@ -3,23 +3,22 @@ import { setWaiting, clearWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 import requestor from '../../../../consume'
 
-export function addEvent(event, navigateTo, consume = requestor) {
-  const storeState = getState()
-  const { gardenId, token } = storeState.user
-  const newEvent = {
-    gardenId,
-    ...event,
+export async function addEvent(event, navigateTo, consume = requestor) {
+  try {
+    const storeState = getState()
+    const { gardenId, token } = storeState.user
+    const newEvent = {
+      gardenId,
+      ...event,
+    }git 
+    dispatch(setWaiting())
+    await consume('/events', token, 'post', newEvent)
+
+    navigateTo(`/gardens/${gardenId}`)
+  } catch (err) {
+    console.log('there is error: ')
+    dispatch(showError(err.message))
+  } finally {
+    dispatch(clearWaiting())
   }
-  dispatch(setWaiting())
-  return consume('/events', token, 'post', newEvent)
-    .then(() => {
-      navigateTo(`/gardens/${gardenId}`)
-      return null
-    })
-    .catch((err) => {
-      dispatch(showError(err.message))
-    })
-    .finally(() => {
-      dispatch(clearWaiting())
-    })
 }
