0b84305ff92832c54efe5d368ee5dcd9b04dde44
Ford <Fordnicholasjones@gmail.com>
2022-09-15 17:22:48 +1200

    Write some guiding psuedocode and dummy data to get the ball rolling for tomorow

--- client/subcomponents/Cart/cartHelper.js
+++ client/subcomponents/Cart/cartHelper.js
@@ -1,1 +1,27 @@
-import { getProduce } from "../produce/ProduceList/produceHelper";
+import { getProduce } from '../produce/ProduceList/produceHelper'
+import requestor from '../../../consume'
+import { useDispatch, useSelector } from 'react-redux'
+import { setWaiting, clearWaiting } from '../../../slices/waiting'
+import { showError } from '../../../slices/error'
+import { useEffect } from 'react'
+
+// OPERATION STEPPINGSTONE
+const fakeData = [{
+  productId: 22, 
+  name: 'test',
+  price: '$30'
+  quantity: 1,
+
+}]
+//fakeData will be called order: its values will be: product.id, product.name, product.price, qty
+
+export async function fetchProduce() {
+  const dispatch = useDispatch()
+  // const produce = useSelector((state) => state.produce)
+
+  useEffect(() => {
+    ;async () => {
+      await getProduce()
+    }
+  }, [])
+}
