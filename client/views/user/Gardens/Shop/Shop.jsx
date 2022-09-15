a64258e970d98845af8d9b40c67ad69509b6a350
Sebastian <sebapian95@gmail.com>
2022-09-15 12:11:04 +1200

    Added CSS

--- client/views/user/Gardens/Shop/Shop.jsx
+++ client/views/user/Gardens/Shop/Shop.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import  {getProducts}  from './shopHelper.js'
+import { getProducts } from './shopHelper.js'
 
 import GardenHeader from '../../../../subcomponents/gardens/GardenHeader/GardenHeader'
 import useGarden from '../../../../hooks/useGarden'
-import { useEffect } from 'react'
-import { useState } from 'react'
 
 export default function Shop() {
   const { id } = useParams()
   const { name, imageHeaderUrl } = useGarden(id)
-  const [ products , setProducts ] = useState([])
+  const [products, setProducts] = useState([])
 
   useEffect(async () => {
     const product = await getProducts()
     setProducts(product)
-    
   }, [])
 
-  // useEffect(() => {
-  // }, [products])
-  
-  console.log(products);
+  console.log(products)
 
   return (
     <>
       <GardenHeader name={name} url={imageHeaderUrl} />
       <p>A shop will be here</p>
-      <h2>Do Something here</h2>
+      <div className="w-3/12 border-solid border-2 rounded-3xl px-16 py-10">
+        <div className="justify-center">
+          <img src="https://cdn.shopify.com/s/files/1/0386/4602/2188/products/WebVersions4.jpg?v=1650839475&width=832"></img>
+        </div>
+        <div className="text-center">
+          <h5>Small Veggie Box</h5>
+        </div>
+        <div>
+          <h5>Price</h5>
+        </div>
+        <div className="flex justify-evenly">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            -
+          </button>
+          <p>5</p>
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            +
+          </button>
+        </div>
+        <div>
+          <button className="text-white bg-blue w-full rounded-sm">
+            Add to basket
+          </button>
+        </div>
+      </div>
     </>
   )
 }
--- client/views/user/Gardens/Shop/shopHelper.js
+++ client/views/user/Gardens/Shop/shopHelper.js
@@ -1,12 +1,38 @@
+import requestor from '../../../../consume'
+import { dispatch } from '../../../../store'
+import { clearWaiting, setWaiting } from '../../../../slices/waiting'
+import { showError } from '../../../../slices/error'
+
 export async function getProducts() {
   return await Promise.resolve(() => {
-    return [{ id: 1,
-      productId: 2,
-      name: 'large veggie box',
-      description: 'a large box filled with fresh seasonal produce grown at your local garden',
-      price: 29,
-      image: 'www.linktowhereeverthisimageis.com',
-      stock: 15,
-    }]
+    return [
+      {
+        id: 1,
+        productId: 2,
+        name: 'large veggie box',
+        description:
+          'a large box filled with fresh seasonal produce grown at your local garden',
+        price: 29,
+        image:
+          'https://cdn.shopify.com/s/files/1/0386/4602/2188/products/WebVersions4.jpg?v=1650839475&width=832',
+        stock: 15,
+      },
+    ]
   })
+}
+
+export function getShopProducts(id, consume = requestor) {
+  dispatch(setWaiting())
+
+  return consume(`/shop/${id}`)
+    .then((res) => {
+      dispatch(clearWaiting())
+      return res.body.gardens
+    })
+    .catch((err) => {
+      dispatch(showError(err))
+    })
+    .finally(() => {
+      dispatch(clearWaiting())
+    })
 }
