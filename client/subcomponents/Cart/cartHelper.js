aec2b27e4d80ee9ace50d2a5dc33e86362d30f72
Frey <freymccahonjones@gmail.com>
2022-09-16 11:42:58 +1200

    Add psuedocode, start writing subcomponent cartHelper function

--- client/subcomponents/Cart/cartHelper.js
+++ client/subcomponents/Cart/cartHelper.js
@@ -1,27 +1,40 @@
 import { getProduce } from '../produce/ProduceList/produceHelper'
 import requestor from '../../../consume'
-import { useDispatch, useSelector } from 'react-redux'
 import { setWaiting, clearWaiting } from '../../../slices/waiting'
 import { showError } from '../../../slices/error'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { dispatch } from '../../store'
 
-// OPERATION STEPPINGSTONE
-const fakeData = [{
-  productId: 22, 
-  name: 'test',
-  price: '$30'
-  quantity: 1,
+// OPERATION SECRETSTONE
 
-}]
-//fakeData will be called order: its values will be: product.id, product.name, product.price, qty
+const fakeCart = [
+  {
+    productId: 22,
+    name: 'test',
+    price: '$30',
+    quantity: 1,
+  },
+]
+//fakeCart will be called order: its values will be: product.id, product.name, product.price, qty
+
+// make function fetchProduce
+// define the initial state of the cart using useState
+// inside fetchProduce chnage the initial state of the cart by calling getProduce
+// create a useEffect
+// pass the function that calls getProduce into the useEffect
+// export fetchProduce
 
 export async function fetchProduce() {
-  const dispatch = useDispatch()
-  // const produce = useSelector((state) => state.produce)
-
+  const [fakeCart, setFakeCart] = useState([])
+  dispatch(setWaiting())
   useEffect(() => {
     ;async () => {
-      await getProduce()
+      try {
+        dispatch(clearWaiting())
+        await getProduce() 
+      } catch (error) {
+        dispatch(showError(error.message))
+      }
     }
   }, [])
 }
