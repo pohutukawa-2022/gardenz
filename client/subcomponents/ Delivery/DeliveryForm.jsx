c3bc8af37b06a5be406d72520ed3120f3dd508f4
Xavier <xavierfenton99@gmail.com>
2022-09-16 12:08:45 +1200

    add basic form layout

--- client/subcomponents/ Delivery/DeliveryForm.jsx
+++ client/subcomponents/ Delivery/DeliveryForm.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function DeliveryForm() {
-  return <h1>this is DeliveryForm</h1>
-}
+  const [form, setForm] = useState({
+    street: '',
+    suburb: '',
+    city: '',
+    postcode: '',
+    deliveryInstruc: '',
+  })
+  function handleFormChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
 
+  return (
+    <>
+      <h1>
+        <strong>Delivery Address</strong>
+      </h1>
+      <form>
+        <label>
+          Number and Street:
+          <input
+            type="text"
+            name="street"
+            onChange={handleFormChange}
+            value={form.street}
+          />
+        </label>
+        <label>
+          Suburb:
+          <input
+            type="text"
+            name="street"
+            onChange={handleFormChange}
+            value={form.street}
+          />
+        </label>
+        <label>
+          City:
+          <input
+            type="text"
+            name="street"
+            onChange={handleFormChange}
+            value={form.street}
+          />
+        </label>
+        <label>
+          Postal Code:
+          <input
+            type="text"
+            name="street"
+            onChange={handleFormChange}
+            value={form.street}
+          />
+        </label>
+        <label>
+          Delivery instructions:
+          <input
+            type="text"
+            name="street"
+            onChange={handleFormChange}
+            value={form.street}
+          />
+        </label>
+      </form>
+    </>
+  )
+}
 export default DeliveryForm
