4a5d0f12352e998d210c98264487e997a3fd73fe
Sebastian <sebapian95@gmail.com>
2022-09-16 12:11:25 +1200

    productlist btn decrement doesnt go below 1

--- client/views/user/Gardens/Shop/shop.test.js
+++ client/views/user/Gardens/Shop/shop.test.js
@@ -42,4 +42,24 @@ it('button increment up', () => {
   userEvent.click(button)
   expect(quantityDisplay.textContent).toMatch('2')
 })
+
+it('button decrement doesnt go below one', () => {
+  const product = {
+    id: 1,
+    productId: 2,
+    name: 'large veggie box',
+    description:
+      'a large box',
+    price: 29,
+    image:
+      'https://cdn.shopify.com/s/files/1/0386/4602/2188/products/WebVersions4.jpg?v=1650839475&width=832',
+    stock: 15,
+  }
+  render(<ProductList key={product.id} product={product} />)
+  const button = screen.getByRole('button', { name: '-' })
+  const quantityDisplay = screen.getByRole('heading', { name: '1' })
+
+  userEvent.click(button)
+  expect(quantityDisplay.textContent).toMatch('1')
+})
 })
