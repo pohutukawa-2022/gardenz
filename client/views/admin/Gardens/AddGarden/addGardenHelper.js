5776d89ac86b47a87aa0f8a59dd5d85907a98111
Ford <Fordnicholasjones@gmail.com>
2022-09-16 12:38:22 +1200

    Add the comment that was accidentally deleted back in

--- client/views/admin/Gardens/AddGarden/addGardenHelper.js
+++ client/views/admin/Gardens/AddGarden/addGardenHelper.js
@@ -3,6 +3,7 @@ import { getState, dispatch } from '../../../../store'
 import { clearWaiting, setWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 
+//create a post api function to post to the server-side-route
 export async function addGarden(data, consume = requestor) {
   const storeState = getState()
   const { token } = storeState.user
