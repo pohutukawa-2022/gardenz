778b339b7dde9f1d862222486030170475dfd432
Vanessa <vanessagur@gmail.com>
2022-09-19 11:06:00 +1200

    Compnent complete but test will be written later

--- client/views/user/Gardens/Gallery/Gallery.test.js
+++ client/views/user/Gardens/Gallery/Gallery.test.js
@@ -1,22 +1,6 @@
-import Gallery from './Gallery'
-// import { screen } from '@testing-library/react'
-// import React from 'react'
-// import '@testing-library/jest-dom'
-
-// import { renderWithRedux } from '../../../../test-utils'
-// import store from '../../../../store'
+// test deliberately left blank
 
 describe('Gallery', () => {
-  // it('First image in Gallery must have src = https://thumbs.dreamstime.com/z/people-garden-7368492.jpg and alt = "garden"', () => {
-  //   renderWithRedux(<Gallery />, { store, initialState: { garden: {} } })
-
-  //   const firstImage = screen.getAllByRole('img')[0]
-  //   expect(firstImage).toHaveAttribute(
-  //     'src',
-  //     'https://thumbs.dreamstime.com/z/people-garden-7368492.jpg'
-  //   )
-  //   expect(firstImage).toHaveAttribute('alt', 'garden')
-  // })
   it('test', () => {
     expect(true).toBeTruthy()
   })
