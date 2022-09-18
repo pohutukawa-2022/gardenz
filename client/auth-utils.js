2d8a4077f4dbb13c3d9c6c2a71ccbf380f0f0103
Sophia Lawler <sophialawler@Sophias-MacBook-Pro.local>
2022-09-19 11:54:23 +1200

    deleting redirect url

--- client/auth-utils.js
+++ client/auth-utils.js
@@ -37,10 +37,8 @@ export function getLoginFn(useAuth0) {
 
 export function getRegisterFn(useAuth0) {
   const { loginWithRedirect } = useAuth0()
-  const redirectUri = `${window.location.origin}/profile`
   return () =>
     loginWithRedirect({
-      redirectUri,
       screen_hint: 'signin',
       scope: 'role:member',
     })
