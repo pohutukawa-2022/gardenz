7acc85a16faa177c6ced222312f7e2d7be37383f
AL <alqianhe7@gmail.com>
2022-09-19 11:18:59 +1200

    styled form

--- client/subcomponents/ Delivery/DeliveryForm.jsx
+++ client/subcomponents/ Delivery/DeliveryForm.jsx
@@ -6,7 +6,7 @@ function DeliveryForm() {
     suburb: '',
     city: '',
     postcode: '',
-    deliveryInstruc: '',
+    deliveryInstructions: '',
   })
   function handleFormChange(event) {
     setForm({ ...form, [event.target.name]: event.target.value })
@@ -14,7 +14,7 @@ function DeliveryForm() {
 
   return (
     <>
-      <div className="p-32 shadow-lg m-20 border-4  ">
+      {/* <div className="p-32 shadow-lg m-20 border-4  ">
         <h1 className="font-bold">Delivery Address</h1>
         <form className="form-content ">
           <label>
@@ -68,7 +68,92 @@ function DeliveryForm() {
             />
           </label>
         </form>
-      </div>
+      </div> */}
+      <form className="w-full max-w-lg">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-number-street"
+            >
+              Number and Street
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="street"
+              onChange={handleFormChange}
+              value={form.street}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-suburb"
+            >
+              Suburb
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="suburb"
+              onChange={handleFormChange}
+              value={form.suburb}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-city"
+            >
+              City
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="city"
+              onChange={handleFormChange}
+              value={form.city}
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-postcode"
+            >
+              Postal Code
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="postcode"
+              onChange={handleFormChange}
+              value={form.postcode}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-instructions"
+            >
+              Delivery Instructions
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="deliveryInstructions"
+              onChange={handleFormChange}
+              value={form.deliveryInstructions}
+            />
+          </div>
+        </div>
+      </form>
     </>
   )
 }
