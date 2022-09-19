caa07ee00e3d768a67b571a322f407ae90d6547a
Alex Coaton <alex.coaton@devacademy.co.nz>
2022-09-20 11:22:38 +1200

    Delete button confirm test.

--- client/views/admin/Gardens/Gallery/AdminGalleryImage.test.js
+++ client/views/admin/Gardens/Gallery/AdminGalleryImage.test.js
@@ -1,85 +1,24 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import { renderWithRedux } from '../../../../test-utils'
+import AdminGalleryImage from './AdminGalleryImage'
 
-import Index from './AdminGallery'
-import { getAllGardens } from './AdminGalleryHelper'
+describe('Interactions with buttons on GalleryImage.', () => {
+  it('Shows confirm dialog when delete button is pressed.', async () => {
+    const image = {
+      id: 1,
+      name: 'image1',
+      url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/dji-0065_orig.jpg',
+      garden_id: 1,
+    }
 
-jest.mock('./IndexHelper')
+    renderWithRedux(<AdminGalleryImage loadImages={() => {}} image={image} />)
 
-afterEach(() => {
-  getAllGardens.mockClear()
-})
+    const deleteButton = await screen.findByTestId('delete-button')
+    userEvent.click(deleteButton)
 
-describe('list of gardens', () => {
-  it('displays information corrctly when array is empty', async () => {
-    getAllGardens.mockImplementation(() => {
-      return Promise.resolve([])
-    })
-
-    renderWithRedux(<Index />)
-
-    const list = await screen.findByRole('list')
-    expect(list).toBeEmptyDOMElement()
-  })
-
-  it('displays information corrctly when array has one object', () => {
-    getAllGardens.mockImplementation(() => {
-      return Promise.resolve([
-        {
-          id: 0,
-          name: 'Test Garden',
-          address: '123 Sesame St',
-          description: 'A test run garden for testing out gardening.',
-          lat: 123,
-          lon: 321,
-          url: 'https://www.google.com/',
-        },
-      ])
-    })
-
-    renderWithRedux(<Index />)
-
-    return screen.findAllByRole('listitem').then((listItems) => {
-      expect(listItems).toHaveLength(1)
-      expect(listItems[0].textContent).toMatch('Test Garden')
-      expect(getAllGardens).toHaveBeenCalledTimes(1)
-      return null
-    })
-  })
-
-  it('displays information corrctly when array has more than one object', () => {
-    getAllGardens.mockImplementation(() => {
-      return Promise.resolve([
-        {
-          id: 0,
-          name: 'Test Garden',
-          address: '123 Sesame St',
-          description: 'A test run garden for testing out gardening.',
-          lat: 123,
-          lon: 321,
-          url: 'https://www.google.com/',
-        },
-        {
-          id: 1,
-          name: 'Test Garden 2',
-          address: '234 Sesame St',
-          description: 'A second test run garden for testing out gardening.',
-          lat: 234,
-          lon: 432,
-          url: 'https://www.google.com/',
-        },
-      ])
-    })
-
-    renderWithRedux(<Index />)
-
-    return screen.findAllByRole('listitem').then((listItems) => {
-      expect(listItems).toHaveLength(2)
-      expect(listItems[0].textContent).toMatch('Test Garden')
-      expect(listItems[1].textContent).toMatch('Test Garden 2')
-      expect(getAllGardens).toHaveBeenCalledTimes(1)
-      return null
-    })
+    const confirmDialog = await screen.findByText('Delete?')
+    expect(confirmDialog).toBeInTheDocument()
   })
 })
