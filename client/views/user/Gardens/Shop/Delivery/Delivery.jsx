42de6ee70096251514eb27438cf46c57c28dec68
AL Lin <alqianhe7@gmail.com>
2022-09-19 14:18:42 +1200

    dropdown

--- client/views/user/Gardens/Shop/Delivery/Delivery.jsx
+++ client/views/user/Gardens/Shop/Delivery/Delivery.jsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import DeliveryForm from '../../../../../subcomponents/Delivery/DeliveryForm'
+
+import 'react-dropdown/style.css'
 
 function Delivery() {
   const [form, setForm] = useState(true)
-  // checked button
+
   const [selected, setSelected] = useState('checked')
+
+  const [day, setDay] = useState('')
 
   function handleCheck(event) {
     setSelected(event.target.value)
@@ -17,46 +21,63 @@ function Delivery() {
     return setForm(false)
   }
 
+  function handleChangeDay(event) {
+    setDay(event.target.value)
+  }
   return (
     <div>
       <div className="rounded-lg border-2 mx-80 my-20 w-full max-w-lg p-16 shadow-md">
-        <div className="flex items-center mb-4 font-sans">
-          <input
-            onClick={() => handleChange()}
-            id="default-radio-1"
-            type="radio"
-            value="checked"
-            checked={selected === 'checked'}
-            onChange={handleCheck}
-            name="default-radio"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            htmlFor="default-radio-1"
-            className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Delivery
-          </label>
+        <div>
+          <div className="flex items-center mb-4 font-sans">
+            <input
+              onClick={() => handleChange()}
+              id="default-radio-1"
+              type="radio"
+              value="checked"
+              checked={selected === 'checked'}
+              onChange={handleCheck}
+              name="default-radio"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="default-radio-1"
+              className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Delivery
+            </label>
+          </div>
+          <div className="flex items-center mb-4 font-sans">
+            <input
+              onClick={() => handleChange2()}
+              id="default-radio-1"
+              type="radio"
+              value="maybe"
+              checked={selected === 'maybe'}
+              onChange={handleCheck}
+              name="default-radio"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="default-radio-1"
+              className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Pick up
+            </label>
+          </div>
+          <div className="w-1/2">
+            <label>
+              Select a delivery day
+              <select value={day} onChange={handleChangeDay}>
+                <option value="monday">Monday</option>
+                <option value="tuesday">Tuesday</option>
+                <option value="wednesday">Wednesday</option>
+                <option value="thursday">Thursday</option>
+                <option value="friday">Friday</option>
+              </select>
+            </label>
+          </div>
         </div>
-        <div className="flex items-center mb-4 font-sans">
-          <input
-            onClick={() => handleChange2()}
-            id="default-radio-1"
-            type="radio"
-            value="maybe"
-            checked={selected === 'maybe'}
-            onChange={handleCheck}
-            name="default-radio"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            htmlFor="default-radio-1"
-            className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Pick up
-          </label>
-        </div>
-        {form ? <DeliveryForm /> : 'pick up form yet to be built '}
+        {form ? <DeliveryForm /> : 'pick up available Monday to Friday '}
       </div>
     </div>
   )
