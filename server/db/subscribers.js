55636d50cdaaa76cc3a2b654dc48c2377cb9d1c7
Gracie <gg.hhayward@gmail.com>
2022-09-19 16:42:00 +1200

    database function

---
+++ server/db/subscribers.js
@@ -0,0 +1,20 @@
+const connection = require('./connection')
+
+module.exports = {
+  getAllSubscribers,
+  createSubscriber,
+}
+
+function getAllSubscribers(id, db = connection) {
+  return db('subscribers').select()
+}
+
+function createSubscriber(subscriberData, db = connection) {
+  const { id, firstName, lastName, email } = subscriberData
+  return db('subscribers').insert({
+    id,
+    first_name: firstName,
+    last_name: lastName,
+    email,
+  })
+}
