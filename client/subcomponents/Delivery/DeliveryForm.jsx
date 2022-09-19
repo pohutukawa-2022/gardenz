9c83994146fe2561d49f75bc620b3fe58229b1a4
AL Lin <alqianhe7@gmail.com>
2022-09-19 12:09:59 +1200

    style form more

--- client/subcomponents/Delivery/DeliveryForm.jsx
+++ client/subcomponents/Delivery/DeliveryForm.jsx
@@ -14,62 +14,7 @@ function DeliveryForm() {
 
   return (
     <>
-      {/* <div className="p-32 shadow-lg m-20 border-4  ">
-        <h1 className="font-bold">Delivery Address</h1>
-        <form className="form-content ">
-          <label>
-            Number and Street:
-            <input
-              className="rounded border-2 border-blue"
-              type="text"
-              name="street"
-              onChange={handleFormChange}
-              value={form.street}
-            />
-          </label>
-          <label>
-            Suburb:
-            <input
-              className="rounded border-2 border-blue"
-              type="text"
-              name="street"
-              onChange={handleFormChange}
-              value={form.street}
-            />
-          </label>
-          <label>
-            City:
-            <input
-              className="rounded border-2 border-blue"
-              type="text"
-              name="street"
-              onChange={handleFormChange}
-              value={form.street}
-            />
-          </label>
-          <label>
-            Postal Code:
-            <input
-              className="rounded border-2 border-blue"
-              type="text"
-              name="street"
-              onChange={handleFormChange}
-              value={form.street}
-            />
-          </label>
-          <label>
-            Delivery instructions:
-            <input
-              className="rounded border-2 border-blue"
-              type="text"
-              name="street"
-              onChange={handleFormChange}
-              value={form.street}
-            />
-          </label>
-        </form>
-      </div> */}
-      <form className="w-full max-w-lg">
+      <form>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label
@@ -79,7 +24,7 @@ function DeliveryForm() {
               Number and Street
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-white-200 text-gray-700 border-2 border-blue rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               name="street"
               onChange={handleFormChange}
@@ -96,7 +41,7 @@ function DeliveryForm() {
               Suburb
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-white-200 text-gray-700 border-2 border-blue rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               name="suburb"
               onChange={handleFormChange}
@@ -113,7 +58,7 @@ function DeliveryForm() {
               City
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-white-200 text-gray-700 border-2 border-blue rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               name="city"
               onChange={handleFormChange}
@@ -128,7 +73,7 @@ function DeliveryForm() {
               Postal Code
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-white-200 text-gray-700 border-2 border-blue rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               name="postcode"
               onChange={handleFormChange}
@@ -145,7 +90,7 @@ function DeliveryForm() {
               Delivery Instructions
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="h-full appearance-none block w-full bg-white-200 text-gray-700 border-2 border-blue rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               name="deliveryInstructions"
               onChange={handleFormChange}
--- client/views/user/Gardens/Shop/Delivery/Delivery.jsx
+++ client/views/user/Gardens/Shop/Delivery/Delivery.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import DeliveryForm from '../../../../../subcomponents/ Delivery/DeliveryForm'
+import DeliveryForm from '../../../../../subcomponents/Delivery/DeliveryForm'
 
 function Delivery() {
   const [form, setForm] = useState(true)
@@ -18,45 +18,47 @@ function Delivery() {
   }
 
   return (
-    <>
-      <div className="flex items-center mb-4 font-sans">
-        <input
-          onClick={() => handleChange()}
-          id="default-radio-1"
-          type="radio"
-          value="checked"
-          checked={selected === 'checked'}
-          onChange={handleCheck}
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          htmlFor="default-radio-1"
-          className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Delivery
-        </label>
+    <div>
+      <div className="rounded-lg border-2 mx-80 my-20 w-full max-w-lg p-16 shadow-md">
+        <div className="flex items-center mb-4 font-sans">
+          <input
+            onClick={() => handleChange()}
+            id="default-radio-1"
+            type="radio"
+            value="checked"
+            checked={selected === 'checked'}
+            onChange={handleCheck}
+            name="default-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="default-radio-1"
+            className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Delivery
+          </label>
+        </div>
+        <div className="flex items-center mb-4 font-sans">
+          <input
+            onClick={() => handleChange2()}
+            id="default-radio-1"
+            type="radio"
+            value="maybe"
+            checked={selected === 'maybe'}
+            onChange={handleCheck}
+            name="default-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="default-radio-1"
+            className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Pick up
+          </label>
+        </div>
+        {form ? <DeliveryForm /> : 'pick up form yet to be built '}
       </div>
-      <div className="flex items-center mb-4 font-sans">
-        <input
-          onClick={() => handleChange2()}
-          id="default-radio-1"
-          type="radio"
-          value="maybe"
-          checked={selected === 'maybe'}
-          onChange={handleCheck}
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          htmlFor="default-radio-1"
-          className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Pick up
-        </label>
-      </div>
-      {form ? <DeliveryForm /> : 'pick up form yet to be built '}
-    </>
+    </div>
   )
 }
 
