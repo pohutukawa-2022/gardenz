2cd43e7812176d603df920735ad4a8b1a4e5a87a
Sophia Lawler <sophialawler@Sophias-MacBook-Pro.local>
2022-09-20 15:30:25 +1200

    news title styling

--- client/views/user/Gardens/about/Description.jsx
+++ client/views/user/Gardens/about/Description.jsx
@@ -4,7 +4,7 @@ const Description = ({ name, description }) => {
   return (
     <article className="lg:w-full rounded-lg shadow-lg p-4 pb-6">
       <p>
-        <span className="font-extrabold">{name} </span>
+        <span className="font-extrabold font-sans text-2xl">{name} </span>
         {description}
       </p>
     </article>
