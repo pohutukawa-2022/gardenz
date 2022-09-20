c27cccf8f3a530697132ea37ad7af150829f9073
Sophia Lawler <sophialawler@Sophias-MacBook-Pro.local>
2022-09-20 14:28:40 +1200

    fix test - removing comments

--- client/views/user/Gardens/about/about.test.js
+++ client/views/user/Gardens/about/about.test.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import { screen, waitFor } from '@testing-library/react'
-
+import { getGarden } from '../../../../hooks/useGarden/useGardenHelper'
 import { renderWithRedux } from '../../../../test-utils'
 import About from './About'
-import { getGarden } from '../../../../hooks/useGarden/useGardenHelper'
-import { getProduce } from '../../../../subcomponents/produce/ProduceList/produceHelper'
 
 jest.mock('../../../../hooks/useGarden/useGardenHelper')
-jest.mock('../../../../subcomponents/produce/ProduceList/produceHelper')
-
-getProduce.mockImplementation(() =>
-  Promise.resolve({
-    produce: [{ id: 1, name: 'banana' }],
-  })
-)
 
 describe('Garden', () => {
   it('calls getGarden helper and displays garden data on mount', async () => {
@@ -36,9 +27,6 @@ describe('Garden', () => {
         },
       },
     })
-
-    // we need renderWithRedux even though Garden isn't connecting to the store
-    // because it's child component (Events) does
     await waitFor(() => {
       const phone = screen.getByText('09 123 4567')
       const email = screen.getByText(/test@test.com/)
