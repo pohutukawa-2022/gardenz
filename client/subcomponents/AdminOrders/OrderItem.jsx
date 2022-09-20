a94e6a7a105f8459aa32ba03f30fd924005872ea
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-09-20 14:15:56 +1200

    ready for peer review

--- client/subcomponents/AdminOrders/OrderItem.jsx
+++ client/subcomponents/AdminOrders/OrderItem.jsx
@@ -6,38 +6,52 @@ export function OrderItem({ order }) {
 
   async function updateOrderStatus(newOrderStatus) {
     await updateOrder(order.id, newOrderStatus)
-    // await fetchOrdersByGarden(3)
     setStatus(newOrderStatus)
   }
 
   return (
     <>
-      <p>Order #{order.id}</p>
-      <p>Order Placed: {order.createdAt}</p>
-      <p>Status: {status}</p>
-      <p>Product List</p>
-
-      {order.produce.map((product) => {
-        return (
-          <div key={product.id}>
-            <p>Product Name: {product.name}</p>
-            <p>Product Quantity: {product.quantity}</p>
-
-            <button
-              onClick={() => updateOrderStatus('Cancelled')}
-              className="order-button"
-            >
-              Cancel Order
-            </button>
-            <button
-              onClick={() => updateOrderStatus('Completed')}
-              className="order-button button-primary"
-            >
-              Order Received
-            </button>
-          </div>
-        )
-      })}
+      <div className="flex flex-col mx-4">
+        <div className="mb-10">
+          <p className="font-extrabold">Order #{order.id}</p>
+          <p>
+            {status === 'Completed' ? '🟢' : '🔴'} Order Placed:
+            {order.createdAt}
+          </p>
+          <p>Status: {status}</p>
+        </div>
+        <div className="flex flex-row justify-between mb-5 font-extrabold">
+          <p>Products:</p>
+          <p>Quantity</p>
+        </div>
+        {order.produce.map((product) => {
+          return (
+            <>
+              <div className="flex flex-row justify-between py-5 mb-10 border-y-2 border-slate-300">
+                <p>{product.name}</p>
+                <p>{product.quantity}</p>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  onClick={() => updateOrderStatus('Completed')}
+                  className="order-button button-primary py-1 bg-orange
+                  rounded-lg text-white mr-6 px-4 sm:py-2 sm:px-6 hover:bg-lightGreen"
+                >
+                  Order Received
+                </button>
+                <button
+                  onClick={() => updateOrderStatus('Cancelled')}
+                  className="order-button py-1
+                  rounded-lg text-black border-black border-2 px-4 1
+                  sm:py-2 sm:px-4 hover:bg-orange hover:text-white hover:border-transparent"
+                >
+                  Cancel Order
+                </button>
+              </div>
+            </>
+          )
+        })}
+      </div>
     </>
   )
 }
--- client/subcomponents/AdminOrders/OrderList.jsx
+++ client/subcomponents/AdminOrders/OrderList.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import OrderItem from './OrderItem'
 
 export function OrderList({ orders }) {
-  return orders.map((order) => <OrderItem key={order.id} order={order} />)
+  return orders.map((order) => (
+    <div key={order.id} className="container mx-auto mt-20 text-lg">
+      <OrderItem order={order} />
+    </div>
+  ))
 }
 
 export default OrderList
